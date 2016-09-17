5022e851294f9ff3eaebd62f4fb30a292c179f37
deoliang <deonyuhaoliang@gmail.com>
2016-09-17 05:48:34 +0000

    Read output from motion sensor for when person is in range of sensor till out of range

--- main.ino
+++ main.ino
@@ -3,11 +3,24 @@
 #define DURT 5000
 #define TIBTWSEND 600000
 
+int motion_started;
+int motion_ended;
+
 void sendEmail() {
 }
 
 void setup() {
+    motion_started = 0;
+    motion_ended = 0;
 }
 
 void loop() {
+    int rval = analogRead(PIRPIN);
+    
+    if(rval > THRESH) {
+        motion_started = 1;
+    }else{
+        motion_ended = 1;
+    }
+    delay(DURT);
 }
