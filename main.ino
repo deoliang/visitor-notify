ef8a14abde8af819affdb2a4e27223dc3ab6bd36
Deon Liang <deoliang@users.noreply.github.com>
2016-09-23 23:19:15 -0700

    Added logic in detecting motion

--- main.ino
+++ main.ino
@@ -1,10 +1,25 @@
 #define PIRPIN D6
 #define DURT 2000
+#define DELAYFORLEAVE 5000
 
 int flag = 0;
+bool sendeds = false;
+bool sendede = false;
+int motion_started = 0;
+int motion_ended = 0;
 
-void sendStatus(String s) {
-  Particle.publish("motion_sensor", s, PRIVATE);
+void sendStartStatus(String s) {
+  if(sendeds == false) {
+    Particle.publish("motion_sensor", s, PRIVATE);
+    sendeds = true;
+  }
+}
+
+void sendEndStatus(String t) {
+  if(sendede == false) {
+    Particle.publish("motion_sensor", t, PRIVATE);
+    sendede = true;
+  }
 }
 
 void setup() {
@@ -14,12 +29,17 @@ void setup() {
 void loop() {
   if (digitalRead(PIRPIN) == HIGH) {
     flag = 1;
-    sendStatus("start");
+    sendStartStatus("start");
   } else {
     if (flag) {
       flag = 0;
-      sendStatus("end");
+      delay(DELAYFORLEAVE);
+      sendEndStatus("end");
     }
   }
   delay(DURT);
+  if(sendeds == true && sendede == true) {
+    sendeds = false;
+    sendede = false;
+  }
 }
